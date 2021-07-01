c3d7b10ae53462299d4a33e6891b31812ca45945
Akhil-Eaga <akhil.bitsian2013@gmail.com>
2021-07-02 04:24:18 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -19,13 +19,18 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
 
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <SearchBox placeholder="Search Monsters" handleChange={e => this.setState({ searchField: e.target.value })} />
+        <h1 className="heading">Monsters Rolodex </h1>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
